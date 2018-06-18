d2d49835882f2f0749892a489b9a39a5172fdf04
Qiyuan Pu <pqy7172@gmail.com>
2018-06-18 18:22:49 +0800

    complete scull.h

---
+++ chapter3/main.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/types.h>
+#include <linux/proc_fs.h>
+#include <linux/fcntl.h>
+#include <linux/seq_file.h>
+#include <linux/cdev.h>
+
+#include <linux/uaccess.h>
+
+#include "scull.h"
